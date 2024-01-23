6470cefc8672948d5e8ec01cab9508e7c4642c73
bmx <baimingxi424@gmail.com>
2024-01-23 15:47:59 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -110,48 +110,7 @@
   const timestamp = useTimestamp({ offset: 0 });
 
   const network = import.meta.env.VITE_APP_NETWORK;
-  const accountList = ref<any[]>([
-    {
-      address: '0x7198a71060ca44184dd1a22011ab2de5deb2eb2d855824a002bfd069273f0b0b',
-      privateKey: '0x9e3856d5bbd199f1b782e6b2226acb211027295dbac774c0497cfcc6997ec419',
-    },
-    {
-      address: '0x30fc104e060b4d2637b4fddfeb2853f27263ac5a1a6b65143b6a6da9f3e55488',
-      privateKey: '0x781a4f4fd1e2d567692b4adf02da6e94bd37f7b954754e294143ce29ca09d451',
-    },
-    {
-      address: '0x8f68823190a51dc15b1d787dd82ea70c59bf4f8bf66721c19d4ea676f9b8d623',
-      privateKey: '0x5fed1b3f5719e3fa6f1735ed55bf6f843186cca63ea757418d4192f8dbe80a6a',
-    },
-    {
-      address: '0x23f056563915015490c1905ff2bd0bf4339a19a4475625e7376912737fe56732',
-      privateKey: '0x688c773eaf008600a54ee4c51c828e3a8f0b009780973d9f0aff21aed3e6f87c',
-    },
-    {
-      address: '0xc2f527fe97bf19ba6b412e8a2f35eadc89bedfd0817a5ff1783f130024372ed2',
-      privateKey: '0xa7a78bc9e9343b9e92ab9e422339ba11e2804e951f5ca127bfe280a29b3b63c9',
-    },
-    {
-      address: '0xd6aee337b0233a8ab3efd8f7131e5f9fa51ac87119cae5550c5235a3b7dd2f44',
-      privateKey: '0x115b9410a6bce3a527ee1bb253fe65f0293a9d99b40422ac66cc30ca40adabf9',
-    },
-    {
-      address: '0x92462714fb5b438c5dd763b23806995571c42e17c932bc3c9bb3fecdb6c6573e',
-      privateKey: '0x864caa2b5071f47befec1e98445f4922c16b85103b81e7bd7804dfa3da616f65',
-    },
-    {
-      address: '0x1ad63f6dbdb2559ff31a945be8f2e5e06b3cb1f0f2b99db1d4730642e3be9a9a',
-      privateKey: '0x548be70a623dd4aea028fc514c79209ffc60062499412a648cbbf5edb68f90e1',
-    },
-    {
-      address: '0x373bb5ee1519cf5dbe381ae3cc5a25a2ae21c9bf1120f25c265cd6c5889348d4',
-      privateKey: '0xa35bf3ce5b6c493cbad29a45a256e541185da72e9b7b6c66bd7aa806d1905d1e',
-    },
-    {
-      address: '0xcfad87f46b487bd31c4453fad3f77c00594d6af8633f9ddc187b45ca7c1481d5',
-      privateKey: '0xf1ae41ca9d2dc756ed90d5c813b75204265bde166d1f94afabafcc95509df0c6',
-    },
-  ]);
+  const accountList = ref<any[]>([]);
   const logs = ref<string[]>([]);
   const silent = ref<boolean>(false);
 
@@ -227,8 +186,8 @@
   const FIRST_BID_TIME = ref(550);
   const RANK_BID_TIME = ref(30);
 
-  const OPEN_FIRST_BID = ref(true);
-  const OPEN_RANK_BID = ref(true);
+  const OPEN_FIRST_BID = ref(false);
+  const OPEN_RANK_BID = ref(false);
 
   const countdownContinue = ref(true);
   const countdownTrigger = () => {
